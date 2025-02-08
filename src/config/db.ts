a2670aa6045833640472043000602ca3cbded91e
Saad Amir <saadamirr12@gmail.com>
2025-02-08 20:18:05 +0400

    fix(database): resolve database connection issue

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,11 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const dbConfig = {
-  host: process.env.DB_HOST || "localhost",
-  port: +(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_DATABASE || "kyc-db",
+  db_url: process.env.DATABASE_URL,
 };
 
 export default dbConfig;
--- src/db/index.ts
+++ src/db/index.ts
@@ -3,34 +3,14 @@ import mysql from "mysql2";
 import dbConfig from "../config/db";
 import * as schema from "./schemas";
 
-const pool = mysql.createPool({
-  host: dbConfig.host,
-  port: dbConfig.port,
-  user: dbConfig.user,
-  password: dbConfig.password,
-  database: dbConfig.database,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const connection = mysql.createConnection({
+  uri: dbConfig.db_url,
 });
-
-// Promisify for async/await usage
-const promisePool = pool.promise();
 
 const db = drizzle({
-  client: promisePool,
+  client: connection,
   schema,
   mode: "default",
-});
-
-// Test connection
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    process.exit(1);
-  }
-  console.log("Successfully connected to database");
-  connection.release();
 });
 
 export default db;
